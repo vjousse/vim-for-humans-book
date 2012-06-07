cd621702275a82d4b15994fc424a78386a93ecbb
Vincent Jousse <vincent@jousse.org>
2012-06-07 21:58:14 +0200

    Move sample book into sample dir

---
+++ book-tex/sample/compile-book.sh
@@ -0,0 +1,7 @@
+pdflatex sample-book
+bibtex sample-book
+texindy --language english sample-book.idx
+# or makeindex sample-book.idx
+pdflatex sample-book
+pdflatex sample-book
+pdflatex sample-book
