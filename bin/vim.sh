9be863f01ea45f49ab01c0d57857309fa2ecabb7
Vincent Jousse <vincent@jousse.org>
2012-07-16 21:49:19 +0200

    Screenshot of the first config

--- bin/vim.sh
+++ bin/vim.sh
@@ -1,2 +1,2 @@
 #export VIM=/home/vjousse/usr/src/txt/vim/vimconfig
-gvim --startuptime out.txt -u /home/vjousse/usr/src/txt/vim/vimconfig/vimrc
+gvim --startuptime out.txt -u /home/vjousse/usr/src/txt/vim-for-humans/firstconfig/vimrc
