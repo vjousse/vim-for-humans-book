7c4fdae61ebc2175ffb86cc24868ac7f698ca14f
Vincent Jousse <vincent@jousse.org>
2013-04-16 09:47:29 +0200

    Modify extract

---
+++ book-tex/extract.sh
@@ -0,0 +1,1 @@
+pdftk vim-pour-les-humains.pdf cat 7-9 output vim-pour-les-humains-extrait.pdf
--- book-tex/make.sh
+++ book-tex/make.sh
@@ -1,3 +1,2 @@
 pdflatex -shell-escape vim-pour-les-humains.tex
-#pdftk vim-pour-les-humains.pdf cat 9-11 output vim-pour-les-humains-extrait.pdf
 evince vim-pour-les-humains.pdf
