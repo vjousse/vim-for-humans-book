45abdca10f011e675f062198a30e4107c27ed520
Vincent Jousse <vincent@jousse.org>
2022-11-18 17:04:59 +0100

    Generate files in fr and en directories

--- makedist.sh
+++ makedist.sh
@@ -13,20 +13,25 @@ make epub
 make latexpdf
 cd ../../dist
 
-# Make Distribution 
+# Make Distribution
 ts=`date +%s`
 mkdir $ts
+mkdir $ts/fr
+mkdir $ts/en
 cd ..
-cp book-tex/vim-pour-les-humains.pdf dist/$ts
-cp rst/fr/_build/epub/Vimpourleshumains.epub dist/$ts/vim-pour-les-humains.epub
-cp rst/fr/_build/epub/Vimpourleshumains.pdf dist/$ts/vim-pour-les-humains-rst.pdf
-cp rst/en/_build/epub/Vimforhumans.epub dist/$ts/vim-for-humans.epub
-cp rst/en/_build/latex/Vimforhumans.pdf dist/$ts/vim-for-humans.pdf
+cp book-tex/vim-pour-les-humains.pdf dist/$ts/fr
+cp rst/fr/_build/epub/Vimpourleshumains.epub dist/$ts/fr/vim-pour-les-humains.epub
+cp rst/fr/_build/epub/Vimpourleshumains.pdf dist/$ts/fr/vim-pour-les-humains-rst.pdf
+cp rst/en/_build/epub/Vimforhumans.epub dist/$ts/en/vim-for-humans.epub
+cp rst/en/_build/latex/Vimforhumans.pdf dist/$ts/en/vim-for-humans.pdf
 cd dist/$ts
 
-# Create Kindle Versions 
+# Create Kindle Versions
+cd fr
 kindlegen vim-pour-les-humains.epub
+cd ../en
 kindlegen vim-for-humans.epub
+cd ..
 
 # Zip
 cd ..
