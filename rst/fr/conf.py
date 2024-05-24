dd6e501421ba9da811360fddcb3dc06f9562212e
Vincent Jousse <vincent@jousse.org>
2024-05-24 09:36:03 +0200

    Update version to 0.2

--- rst/fr/conf.py
+++ rst/fr/conf.py
@@ -12,9 +12,9 @@ author = "Vincent Jousse"
 
 
 # The short X.Y version.
-version = "1.0"
+version = "2.0"
 # The full version, including alpha/beta/rc tags.
-release = "1.0"
+release = "2.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
