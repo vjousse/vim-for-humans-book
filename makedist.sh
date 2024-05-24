8078a3ec7fe70fefed85f8c7fdfb5293aa3f083b
Vincent Jousse <vincent@jousse.org>
2024-05-24 12:27:33 +0200

    Don't create book from latex version anymore

--- makedist.sh
+++ makedist.sh
@@ -13,15 +13,7 @@ make clean
 make epub
 make latexpdf
 
-cd ../../book-tex
-
-# We need to run it twice for the images to appear
-# Don't ask me why 🤓
-pdflatex -shell-escape vim-pour-les-humains.tex
-pdflatex -shell-escape vim-pour-les-humains.tex
-
-
-cd ../dist
+cd ../../dist
 
 # Make Distribution
 ts=`date +%s`
@@ -29,9 +21,8 @@ mkdir $ts
 mkdir $ts/fr
 mkdir $ts/en
 cd ..
-cp book-tex/vim-pour-les-humains.pdf dist/$ts/fr
 cp rst/fr/_build/epub/Vimpourleshumains.epub dist/$ts/fr/vim-pour-les-humains.epub
-cp rst/fr/_build/latex/vimpourleshumains.pdf dist/$ts/fr/vim-pour-les-humains-rst.pdf
+cp rst/fr/_build/latex/vimpourleshumains.pdf dist/$ts/fr/vim-pour-les-humains.pdf
 cp rst/en/_build/epub/Vimforhumans.epub dist/$ts/en/vim-for-humans.epub
 cp rst/en/_build/latex/Vimforhumans.pdf dist/$ts/en/vim-for-humans.pdf
 cd dist/$ts
@@ -46,6 +37,7 @@ cd ..
 # Zip
 cd ..
 rm -rf vimpourleshumains/
-rm -f vimpourleshumains.zip
+rm -f vimpourleshumains.zip vimpourleshumains.tar.gz
 cp -rf $ts vimpourleshumains
 zip -r vimpourleshumains.zip vimpourleshumains
+tar czvf vimpourleshumains.tar.gz vimpourleshumains
